e77c445f6a69ac86a72e18c707e23c223e4a26a7
Rishita Shaw <rishitashaw49@gmail.com>
2022-02-13 14:48:43 +0530

    unique string leetcode

---
+++ sliding_window/unique_string.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string unquie_substring(string str)
+{
+    int i = 0;
+    int j = 0;
+    int window_len = 0;
+    int max_window_len = 0;
+    int start_window = -1;
+
+    unordered_map<char, int> m;
+
+    while (j < str.length())
+    {
+        char ch = str[j];
+
+        if (m.count(ch) and m[ch] >= i)
+        {
+            i = m[ch] + 1;
+            window_len = j - i;
+        }
+        m[ch] = j;
+        window_len++;
+        j++;
+
+        if (window_len >= max_window_len)
+        {
+            max_window_len = window_len;
+            start_window = i;
+        }
+    }
+    return str.substr(start_window, max_window_len);
+}
+
+int main()
+{
+    string str = "prateekbhaiya";
+
+    string result = unquie_substring(str);
+    cout << result << endl;
+}
