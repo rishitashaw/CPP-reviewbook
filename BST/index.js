5d8f1b2bba72ff638520a76bc5080c98a56d29a0
Rishita Shaw <rishitashaw49@gmail.com>
2022-02-28 00:40:21 +0530

    fml

---
+++ BST/index.js
@@ -0,0 +1,30 @@
+var line_1 = {
+    contributor_num="",
+    project_num="",
+
+}
+
+var line_2 = {
+    contributor_name="", // not more than 20 characters
+    skills_num="",
+}
+var line_3 = {
+    skills_name="",
+    skills_level="",
+}
+
+var line_4 = {
+    project_name="",
+    project_days="",
+    scores_for_project="",
+    best_before="",
+    roles="",
+}
+var line_5 = {
+    skill_for_project="",
+    skill_level_for_project=""
+}
+
+var program = {}
+
+program.contributor_num = ""
