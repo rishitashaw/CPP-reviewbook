6edfe1e573db5e34b5cff805aa42de0de7da7957
Rishita Shaw <rishitashaw49@gmail.com>
2022-03-24 19:39:58 +0530

    leetcode

--- arrays and vectors/stocks.cpp
+++ arrays and vectors/stocks.cpp
@@ -1,31 +1,23 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 int maxProfit(vector<int> &prices)
 {
+    int buy = prices[0];
+    int profit = 0;
     int n = prices.size();
-    if (n <= 1)
+
+    for (int i = 1; i < n; i++)
     {
-        return 0;
+        buy = min(buy, prices[i]);
+        profit = max(profit, prices[i] - buy);
     }
-    int j = 0;
-    int k = n - 1;
-    int minVal = INT_MAX;
-    int maxVal = INT_MIN;
-    while (j < k)
-    {
-        minVal = min(prices[j], minVal);
-        maxVal = max(prices[k], maxVal);
-        j++;
-        k--;
-    }
-    if ((maxVal - minVal) < 0)
-        return 0;
-    return maxVal - minVal;
+    return profit;
 }
 
 int main()
 {
-    vector<int> arr{7, 6, 4, 3, 1};
-    cout << maxProfit(arr) << endl;
+    vector<int> prices{7, 1, 5, 3, 6, 4};
+    cout << maxProfit(prices) << endl;
 }
